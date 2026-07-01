6c39959a031143d671d80f37e2865ad52ad4115e
Samuel Petersson <samuel.petersson@hotmail.com>
2026-07-01 11:56:52 +0200

    Färdiga publika sidor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,43 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using projekt.NET.Models;
+using Microsoft.EntityFrameworkCore;
+using projekt.NET.Data;
 
 namespace projekt.NET.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var recept = await _context.Recept.Include(r => r.Kategori).ToListAsync();
+        return View(recept);
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var recept = await _context.Recept.Include(r => r.Kategori).Include(r => r.Ingredienser).FirstOrDefaultAsync(r => r.Id == id);
+
+        if (recept == null)
+        {
+            return NotFound();
+        }
+
+        return View(recept);
     }
 
     public IActionResult Privacy()
